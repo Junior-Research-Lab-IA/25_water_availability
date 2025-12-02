928b734e30f46488be8af26e62b040cd339c2cf3
JeanneTorreele <jeanne.torreele@supagro.fr>
2025-12-02 17:58:09 +0100

    delete

--- airgrccia-main/tests/testthat/test-extract_sub_basins.R
+++
@@ -1,5 +0,0 @@
-test_that("extract_sub_basins works", {
-  expect_s3_class(sb, "sf")
-  expect_true(all(sf::st_geometry_type(sb) == "POLYGON"))
-  expect_true(all(sf::st_area(sb) > units::set_units(1E6, "m^2")))
-})
